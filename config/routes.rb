29e98196126adb44e4a800f739689b869a3fe3d5
Matthew Flood <mflood@shutterstock.com>
2014-12-27 14:31:30 -0500

    heroku part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'maintenance/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'maintenance#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
